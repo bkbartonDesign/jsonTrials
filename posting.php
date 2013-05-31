0ebb9efae767a4e843da5d787971bea23693f6b6
bkbarton <n3w_y0rk@hotmail.com>
2013-05-31 17:07:50 -0500

    Fixed issues with function passing variable.

--- posting.php
+++ posting.php
@@ -1,19 +1,13 @@
 <?php
 	error_reporting(E_ALL);
-  if(isset($_POST)){echo "posted";}
-  else {echo "not posted";}
   
 //  print_r($_POST);
   $json = $_POST;
-	echo $json;
-//	$json = '{"four":"4 / four"}';
-	
-   if ($json != "") { /* sanity check */
+	print_r($json);
 
-//     $json = json_decode($json);     
-     
-	 echo "success";
-     
+	
+   if ($json != "") { /* sanity check */     
+	 echo "success";     
      $fileContents = file_get_contents('data.json');
      $data = json_decode($fileContents);
      unset($fileContents);
@@ -22,9 +16,9 @@
      
      $merged = array_merge((array)$data, (array)$json);
 
-	 print_r($merged);
+//	 print_r($merged);
      file_put_contents('data.json',json_encode($merged));
-     var_dump($data);
+//    var_dump($data);
      unset($data);     
    } 
    else {
